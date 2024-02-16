ac8cf608b3cb760e68c89482d13abdba67f3ca1f
mpsalunggg <putrasatria893@gmail.com>
2024-02-16 21:13:27 +0800

    add: add icon with input

--- src/features/Sign-in/index.tsx
+++ src/features/Sign-in/index.tsx
@@ -1,9 +1,21 @@
+'use client';
 import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { EnvelopeOpenIcon } from '@radix-ui/react-icons';
 
 const SignIn = () => {
   return (
     <div>
       <Button>Sign In</Button>
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <EnvelopeOpenIcon className="mr-2 h-4 w-4" />
+        <Input
+          id="picture"
+          type="text"
+          className="pl-12"
+          icon={<EnvelopeOpenIcon className="absolute left-4" />}
+        />
+      </div>
     </div>
   );
 };
