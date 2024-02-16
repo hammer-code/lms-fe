4b831a58d6c158c4732cd55c0141ea93f0f9da45
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-02-16 23:47:05 +0800

    feat: create card

--- src/features/Main/components/CardPartner/index.tsx
+++ src/features/Main/components/CardPartner/index.tsx
@@ -9,10 +9,12 @@ import { FC } from 'react';
 
 const CardPartners: FC = () => {
   return (
-    <Card>
+    <Card className="w-4/5 h-[200px] rounded-3xl shadow-xl">
       <CardHeader>
-        <CardTitle>Learning From Our Expert Tech</CardTitle>
-        <CardDescription>
+        <CardTitle className="text-[#00CCFF]">
+          Learning From Our Expert Tech
+        </CardTitle>
+        <CardDescription className="text-[#0F3665]">
           Take the opportunity to learn from experts from tech companies
         </CardDescription>
       </CardHeader>
