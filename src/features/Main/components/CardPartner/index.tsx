a78462c331717cbfc4005b9ad735ed2a6d7334e0
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-02-17 23:06:52 +0800

    feat: making card responsive

--- src/features/Main/components/CardPartner/index.tsx
+++ src/features/Main/components/CardPartner/index.tsx
@@ -16,18 +16,23 @@ const CardPartners: FC = () => {
   ];
   return (
     <div className="flex justify-center">
-      <Card className="w-full md:w-4/5 max-w-screen-lg h-[200px] rounded-3xl shadow-xl p-2">
+      <Card className="w-full md:w-4/5 max-w-screen-lg rounded-3xl shadow-xl p-2">
         <CardHeader>
           <CardTitle className="text-[#00CCFF] flex justify-center">
             Learning From Our Expert Tech
           </CardTitle>
-          <CardDescription className="text-[#0F3665] flex justify-center">
+          <CardDescription className="text-[#0F3665] max-w-[80%] md:max-w-full mx-auto md:mx-0 text-center flex justify-center">
             Take the opportunity to learn from experts from tech companies
           </CardDescription>
         </CardHeader>
-        <CardContent className="flex gap-4 justify-center">
+        <CardContent className="flex flex-wrap gap-4 justify-center">
           {Images.map((item, index) => (
-            <img key={index} src={`../assets/img/${item.src}`} alt="" />
+            <img
+              key={index}
+              src={`../assets/img/${item.src}`}
+              alt=""
+              className="max-w-full h-auto rounded"
+            />
           ))}
         </CardContent>
       </Card>
