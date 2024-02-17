8846bc67015bfb2f8304b295edd8e0718f9e4429
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-02-17 22:54:48 +0800

    feat: mapping images

--- src/features/Main/components/CardPartner/index.tsx
+++ src/features/Main/components/CardPartner/index.tsx
@@ -8,18 +8,30 @@ import {
 import { FC } from 'react';
 
 const CardPartners: FC = () => {
+  const Images = [
+    { src: 'efishery.png' },
+    { src: 'icehouse.png' },
+    { src: 'learning.png' },
+    { src: 'shipper.png' }
+  ];
   return (
-    <Card className="w-4/5 h-[200px] rounded-3xl shadow-xl">
-      <CardHeader>
-        <CardTitle className="text-[#00CCFF]">
-          Learning From Our Expert Tech
-        </CardTitle>
-        <CardDescription className="text-[#0F3665]">
-          Take the opportunity to learn from experts from tech companies
-        </CardDescription>
-      </CardHeader>
-      <CardContent></CardContent>
-    </Card>
+    <div className="flex justify-center">
+      <Card className="w-full md:w-4/5 max-w-screen-lg h-[200px] rounded-3xl shadow-xl p-2">
+        <CardHeader>
+          <CardTitle className="text-[#00CCFF] flex justify-center">
+            Learning From Our Expert Tech
+          </CardTitle>
+          <CardDescription className="text-[#0F3665] flex justify-center">
+            Take the opportunity to learn from experts from tech companies
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex gap-4 justify-center">
+          {Images.map((item, index) => (
+            <img key={index} src={`../assets/img/${item.src}`} alt="" />
+          ))}
+        </CardContent>
+      </Card>
+    </div>
   );
 };
 
