8eacc31c43ed4fa5d1a1a8f46301fb693be26009
mpsalunggg <putrasatria893@gmail.com>
2024-02-17 23:44:21 +0800

    fix: ssr, foldering components

--- src/features/Main/index.tsx
+++ src/features/Main/index.tsx
@@ -1,4 +1,8 @@
-import Navbar from '@/components/Navbar';
+import dynamic from 'next/dynamic';
+
+const Navbar = dynamic(() => import('../../components/layout/Navbar'), {
+  ssr: false
+});
 
 const Main = () => {
   return (
