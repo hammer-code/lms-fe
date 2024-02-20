dbecfa8899ec4fdcf8750a8281776c6c08428272
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-02-20 18:33:13 +0800

    fix: edit radius

--- src/features/Main/components/OurClass/index.tsx
+++ src/features/Main/components/OurClass/index.tsx
@@ -19,18 +19,19 @@ const OurClass = () => {
             className="flex gap-8 flex-wrap justify-center"
           >
             {imagecarousel.map((image, index) => (
-              <Card key={index} className="w-[296px] h-[428px] p-3 shadow-xl">
+              <Card key={index} className="w-[296px] h-[428px] shadow-xl">
                 <Image
                   src={`/../assets/imgcarousel/${image.src}`}
                   width={300}
                   height={100}
                   alt={''}
+                  className="rounded-t-md"
                 />
                 <h1 className="text-left font-bold text-[#0F3665]">
                   Product Management Basic - Course
                 </h1>
                 <p className="text-left text-[10px]">
-                  Product Management Masterclass, you will learn with Sar ah
+                  Product Management Masterclass, you will learn with Sarah
                   Johnson - Head of Product Customer Platform Gojek Indonesia.
                 </p>
                 <button className="w-[102px] h-[38px] text-white bg-[#0F3665] text-[12px] rounded-md">
