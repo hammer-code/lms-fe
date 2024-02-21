1be953ea68aefd3fbfbc2a932be7160ecaab372d
Farhan <anak.upload90@gmail.com>
2024-02-21 23:44:31 +0800

    feat: Create constant for social media

---
+++ src/constants/socialMedia.ts
@@ -0,0 +1,14 @@
+import { DiscordLogoIcon, InstagramLogoIcon } from '@radix-ui/react-icons';
+
+export const SocialMedia= [
+  {
+    title: 'Instagram',
+    icon: InstagramLogoIcon,
+    url: 'https://www.instagram.com/hmrcode/'
+  },
+  {
+    title: 'Discord',
+    icon: DiscordLogoIcon,
+    url: '#'
+  }
+];
