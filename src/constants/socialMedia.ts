b6e34247a4cfa75fe03457215624724db683f34f
Farhan <anak.upload90@gmail.com>
2024-02-22 22:05:00 +0800

    chore: Update bgColor property on Social Media data

--- src/constants/socialMedia.ts
+++ src/constants/socialMedia.ts
@@ -1,15 +1,17 @@
 import { SocialMediaType } from '@/types/types';
 import { DiscordLogoIcon, InstagramLogoIcon } from '@radix-ui/react-icons';
 
-export const SocialMedia: SocialMediaType[]= [
-  {
-    title: 'Instagram',
-    icon: InstagramLogoIcon,
-    url: 'https://www.instagram.com/hmrcode/'
-  },
+export const SocialMedia: SocialMediaType[] = [
   {
     title: 'Discord',
     icon: DiscordLogoIcon,
+    bgColor: 'bg-[#5865F2]',
     url: '#'
+  },
+  {
+    title: 'Instagram',
+    icon: InstagramLogoIcon,
+    bgColor: 'bg-gradient-to-br from-[#9A34C1] to-[#FF5746]',
+    url: 'https://www.instagram.com/hmrcode/'
   }
 ];
