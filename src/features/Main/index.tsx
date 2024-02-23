20a3dec5c2d524142b5e29760d0e99f7cee4a810
Farhan <anak.upload90@gmail.com>
2024-02-23 08:41:45 +0800

    feat(main): Create Hero section

--- src/features/Main/index.tsx
+++ src/features/Main/index.tsx
@@ -1,10 +1,12 @@
 import Navbar from '@/components/layout/Navbar';
 import CardPartners from './components/CardPartner';
+import Hero from './sections/Hero';
 
 const Main = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
       <CardPartners />
     </div>
   );
--- src/features/Main/sections/Hero/index.tsx
+++ src/features/Main/sections/Hero/index.tsx
@@ -1,6 +1,28 @@
+import Image from 'next/image';
+import HeroImage from '../../../../../public/assets/img/Hero.svg';
 import { FC } from 'react';
 
+
 const Hero: FC = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between my-6">
+      <div className=" md:w-2/5 flex flex-col gap-6">
+        <h1 className="text-5xl font-semibold text-[#0F3665]">
+          Improve Your Skill by Talking High-Quality Classes
+        </h1>
+        <p className="text-sm">
+          <strong>HMCLearn</strong> is a streamlined and user-friendly Learning
+          Management System (LMS) dedicated to efficient learning material
+          management and accessibility.
+        </p>
+        <div className="flex gap-3">
+          
+        </div>
+      </div>
+      <div className="md:w-1/2">
+        <Image src={HeroImage} alt="Hero Image" />
+      </div>
+    </div>
+  );
 };
 export default Hero;
