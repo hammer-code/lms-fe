0d3db20c3783b2b9e8cf61d90f309b20bead51fc
Farhan <anak.upload90@gmail.com>
2024-02-23 13:59:07 +0800

    refactor: Make header text minimize when responsive

--- src/features/Main/sections/Hero/index.tsx
+++ src/features/Main/sections/Hero/index.tsx
@@ -8,7 +8,7 @@ const Hero: FC = () => {
   return (
     <div className="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between my-6">
       <div className=" md:w-2/5 flex flex-col gap-6">
-        <h1 className="text-5xl font-semibold text-[#0F3665]">
+        <h1 className="text-4xl md:text-5xl font-semibold text-[#0F3665]">
           Improve Your Skill by Talking High-Quality Classes
         </h1>
         <p className="text-sm">
