8a4b265619555310f43f8ac2f64822ce2fb229d2
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-02-23 13:36:34 +0800

    fix: import images

--- src/constants/carousel.ts
+++ src/constants/carousel.ts
@@ -1,12 +1,11 @@
 import { ImageCarouselType } from '@/types/types';
 import { LogoCarouselType } from '@/types/types';
-import { TextCarouselType } from '@/types/types';
 
 export const imagecarousel: ImageCarouselType[] = [
   { id: 1, alt: 'temporary', src: 'test.svg' },
   { id: 2, alt: 'temporary', src: 'test1.svg' },
-  { id: 3, alt: 'temporary', src: 'figma.svg' },
-  { id: 4, alt: 'temporary', src: 'figma.svg' },
+  { id: 3, alt: 'temporary', src: 'figmayes.svg' },
+  { id: 4, alt: 'temporary', src: 'figmaa.svg' },
   { id: 5, alt: 'temporary', src: 'test.svg' },
   { id: 6, alt: 'temporary', src: 'test1.svg' },
   { id: 7, alt: 'temporary', src: 'test1.svg' }
