3df8a46f2b9a8c523816326fde1d9f5b305aca6e
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-02-29 09:52:25 +0800

    fix: custom color

--- src/features/Main/components/OurClassText/index.tsx
+++ src/features/Main/components/OurClassText/index.tsx
@@ -4,7 +4,7 @@ const OurClassText: FC = () => {
   return (
     <div className="flex justify-center items-center my-10 mx-4 md:mx-2 ">
       <div className="text-wrap w-[750px]">
-        <h1 className="font-semibold text-[#00CCFF] text-center text-[20px] md:text-[30px]">
+        <h1 className="font-semibold text-primary-blue text-center text-2xl md:text-[30px]">
           Our Class
         </h1>
         <p className="text-center mt-2 text-[#828282] ">
