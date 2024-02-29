85d693c50d6eca79ccd78914eea358423a370ee4
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-02-29 19:39:08 +0800

    feat: add new button

--- src/features/Main/index.tsx
+++ src/features/Main/index.tsx
@@ -1,6 +1,7 @@
 import Navbar from '../../components/layout/Navbar';
 import AccordionFAQ from './components/AccordionFAQ';
 import CardPartners from './components/CardPartner';
+import LearnMore from './components/LearnMoreButton';
 import OurClass from './components/OurClass';
 import OurClassText from './components/OurClassText';
 import Hero from './sections/Hero';
@@ -13,6 +14,7 @@ const Main = () => {
       <CardPartners />
       <OurClassText />
       <OurClass />
+      <LearnMore />
       <AccordionFAQ />
     </div>
   );
