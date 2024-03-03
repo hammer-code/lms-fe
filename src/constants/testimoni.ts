c9f18ff31ec6372e02de9dc09528503a605ccc2e
Farhan <anak.upload90@gmail.com>
2024-03-03 18:58:44 +0800

    feat: Create testimoni constant

---
+++ src/constants/testimoni.ts
@@ -0,0 +1,15 @@
+import { getAssetUrl } from '@/lib/utils';
+
+export const testimoni = [
+  {
+    name: 'Abdul Rojak',
+    img: getAssetUrl('/img/person.jpg'),
+    comment: `Thank you so much for your help. It\'s exactly what I\'ve been looking for. You won\'t regret it. It really saves me time and effort.`
+  },
+  {
+    name: 'Reina Rey',
+    img: getAssetUrl('/img/person2.jpg'),
+    comment: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    `
+  }
+];
