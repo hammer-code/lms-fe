cd8f553a08153d42e259257f5eae5ac389a185d2
Farhan <anak.upload90@gmail.com>
2024-03-03 19:00:21 +0800

    refactor: mapping testimoni

--- src/features/Main/sections/Testimonial/index.tsx
+++ src/features/Main/sections/Testimonial/index.tsx
@@ -9,6 +9,7 @@ import {
 import { Card } from '@/components/ui/card';
 import Image from 'next/image';
 import { getAssetUrl } from '@/lib/utils';
+import { testimoni } from '@/constants/testimoni';
 
 const Testimonial: FC = () => {
   return (
@@ -33,31 +34,33 @@ const Testimonial: FC = () => {
           className="w-[15.5rem] md:w-[22rem] lg:w-[28rem]"
         >
           <CarouselContent>
-            <CarouselItem>
-              <div className="absolute w-[12rem] h-[16rem] md:w-[15rem] md:h-[24rem] lg:w-[20rem] lg:h-[32rem] rounded-xl drop-shadow-lg">
-                <Image
-                  src={getAssetUrl('/img/person.jpg')}
-                  layout="fill"
-                  objectFit="cover"
-                  alt="image"
-                  className="rounded-xl"
-                />
-              </div>
-              <div className="h-[20rem] w-[15.5rem] md:h-[28rem] md:w-[22rem] lg:h-[36rem] lg:w-[28rem] flex items-end justify-end ">
-                <Card className="w-[13rem] md:w-[19rem] lg:w-[24rem] z-50 rounded-xl">
-                  <div className="border-l-8 border-[#00CCFF] rounded-xl py-4 px-4 md:py-6 md:px-6 lg:py-10 lg:px-8">
-                    <p className="border-l-2 pl-4 md:pl-6 text-xs md:text-base">
-                      Thank you so much for your help. It&apos;s exactly what
-                      I&apos;ve been looking for. You won&apos;t regret it. It
-                      really saves me time and effort.
-                    </p>
-                    <p className="px-4 md:px-6 mt-4 md:mt-6 lg:mt-10 text-xs md:text-base">
-                      Nama Pengguna
-                    </p>
+            {testimoni.map((items, index) => {
+              return (
+                <CarouselItem key={index}>
+                  <div className="absolute w-[12rem] h-[16rem] md:w-[15rem] md:h-[24rem] lg:w-[20rem] lg:h-[32rem] rounded-xl drop-shadow-lg">
+                    <Image
+                      src={items.img}
+                      layout="fill"
+                      objectFit="cover"
+                      alt="image"
+                      className="rounded-xl"
+                    />
                   </div>
-                </Card>
-              </div>
-            </CarouselItem>
+                  <div className="h-[20rem] w-[15.5rem] md:h-[28rem] md:w-[22rem] lg:h-[36rem] lg:w-[28rem] flex items-end justify-end ">
+                    <Card className="w-[13rem] md:w-[19rem] lg:w-[24rem] z-50 rounded-xl">
+                      <div className="border-l-8 border-[#00CCFF] rounded-xl py-4 px-4 md:py-6 md:px-6 lg:py-10 lg:px-8">
+                        <p className="border-l-2 pl-4 md:pl-6 text-xs md:text-base line-clamp-6 ">
+                          {items.comment}
+                        </p>
+                        <p className="px-4 md:px-6 mt-4 md:mt-6 lg:mt-10 text-xs md:text-base">
+                          {items.name}
+                        </p>
+                      </div>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              );
+            })}
           </CarouselContent>
           <CarouselNext className="right-[2.5rem] top-[10rem] md:scale-[1.5] md:right-[6rem] md:top-[11rem] lg:scale-[2] lg:right-[7rem] lg:top-[15rem]" />
         </Carousel>
