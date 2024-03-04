b3c3d2b1aa4914ae517efcfd529b78b627456f47
zkyzzz <muhadlim17@gmail.com>
2024-03-04 22:25:45 +0800

    feat: Slicing SignUp and SignIn Page

---
+++ src/features/Auth/Sign-in/index.tsx
@@ -0,0 +1,53 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import loginBg from '@/assets/auth/login-astronout.svg';
+
+const SignIn = () => {
+  return (
+    <div className="w-screen h-screen p-4">
+      <div className="flex min-h-full relative overflow-hidden justify-center">
+        <div className="flex flex-col items-center justify-center lg:w-4/12">
+          <div className="flex flex-col gap-6 mx-auto lg:w-96">
+            <div className="flex flex-col gap-2">
+              <h1 className="font-bold text-4xl">Sign In</h1>
+              <p className="text-sm text-[#828282] mt-2">
+                Let&apos;s started! Please Sign In to access the best learning
+                resources.
+              </p>
+            </div>
+            <div className="flex flex-col gap-4">
+              <Input
+                type="email"
+                placeholder="Masukkan Email"
+                className="border-[#828282]"
+              />
+              <Input
+                type="password"
+                placeholder="Masukkan Password"
+                className="border-[#828282]"
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Button className="text-white rounded-2xl">Sign In</Button>
+              <p className="text-xs text-center">
+                Don&apos;t have an account yet?{' '}
+                <Link href="" className="text-hmc-base">
+                  Sign Up
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+        <Image
+          priority
+          src={loginBg}
+          alt="Login Background"
+          className="hidden lg:block w-4/12 h-4/12"
+        />
+      </div>
+    </div>
+  );
+};
+export default SignIn;
