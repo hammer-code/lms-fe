c60c0841e35b0e6021dc8d2ca2f55cae603a07a6
mpsalunggg <putrasatria893@gmail.com>
2024-03-09 00:11:42 +0800

    refactor: focus input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -10,12 +10,11 @@ export interface InputProps
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ suffix, className, type, ...props }, ref) => {
-    const [isFocused, setIsFocused] = React.useState(false);
     return (
       <div
         className={cn(
-          'flex items-center gap-2 h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-          isFocused && 'border border-blue-400'
+          'flex items-center gap-2 h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background focus-within:ring-2 focus-within:ring-offset-1',
+          className
         )}
       >
         {suffix}
@@ -23,8 +22,6 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           type={type}
           className={cn('w-full outline-none', className)}
           ref={ref}
-          onFocus={() => setIsFocused(true)}
-          onBlur={() => setIsFocused(false)}
           {...props}
         />
       </div>
