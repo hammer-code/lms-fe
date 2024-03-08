f56a99f0f4a908b8e5313ef50ef3e581f87f9ddd
mpsalunggg <putrasatria893@gmail.com>
2024-03-08 20:26:50 +0800

    feat: setup zod, hook form for auth

--- src/features/Auth/Sign-in/index.tsx
+++ src/features/Auth/Sign-in/index.tsx
@@ -1,17 +1,37 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { LockKeyhole, Mail } from 'lucide-react';
+
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import signInBg from '@/assets/auth/signin-astronout.svg';
-import { LockKeyhole, Mail } from 'lucide-react';
 import { InputPassword } from '@/components/ui/input-password';
+import { LoginSchema, LoginType } from '@/domains/auth';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage
+} from '@/components/ui/form';
 
 const SignIn = () => {
+  const form = useForm<LoginType>({
+    resolver: zodResolver(LoginSchema)
+  });
+
+  const onSubmit = (values: LoginType) => {
+    console.log(values);
+  };
+
   return (
     <div className="w-screen h-screen p-4">
       <div className="flex min-h-full relative overflow-hidden justify-center items-center">
         <div className="flex flex-col items-center justify-center lg:w-4/12">
-          <div className="flex flex-col gap-6 mx-auto lg:w-96">
+          <div className="flex flex-col gap-4 mx-auto lg:w-96">
             <div className="flex flex-col gap-2">
               <h1 className="font-bold text-4xl">Sign In</h1>
               <p className="text-sm text-[#828282] mt-2">
@@ -20,22 +40,53 @@ const SignIn = () => {
               </p>
             </div>
             <div className="flex flex-col gap-4">
-              <Input
-                type="email"
-                placeholder="Enter Email"
-                className="border-[#828282]"
-                suffix={<Mail className="text-[#828282]" />}
-              />
-              <InputPassword
-                type="password"
-                placeholder="Enter Password"
-                className="border-[#828282]"
-                colorIcon="text-[#828282]"
-                suffix={<LockKeyhole className="text-[#828282]" />}
-              />
+              <Form {...form}>
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input
+                          type="email"
+                          placeholder="Enter Email"
+                          className="border-[#828282]"
+                          suffix={<Mail className="text-[#828282]" />}
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <InputPassword
+                          type="password"
+                          placeholder="Enter Password"
+                          className="border-[#828282]"
+                          colorIcon="text-[#828282]"
+                          suffix={<LockKeyhole className="text-[#828282]" />}
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </Form>
             </div>
             <div className="flex flex-col gap-2">
-              <Button className="text-white rounded-2xl">Sign In</Button>
+              <Button
+                className="text-white rounded-2xl"
+                onClick={form.handleSubmit(onSubmit)}
+              >
+                Sign In
+              </Button>
               <p className="text-xs text-center">
                 Don&apos;t have an account yet?{' '}
                 <Link href="/sign-up" className="text-hmc-base">
