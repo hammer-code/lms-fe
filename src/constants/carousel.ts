447eebd6deeac8ba22034b1a5082d375677fb166
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-03-10 14:06:35 +0800

    fix: rename image folder and image file

--- src/constants/carousel.ts
+++ src/constants/carousel.ts
@@ -2,12 +2,12 @@ import { ImageCarouselType } from '@/types/types';
 import { LogoCarouselType } from '@/types/types';
 
 export const imagecarousel: ImageCarouselType[] = [
-  { id: 1, alt: 'temporary', src: 'test.svg' },
-  { id: 2, alt: 'temporary', src: 'test1.svg' },
-  { id: 3, alt: 'temporary', src: 'hello.png' },
-  { id: 4, alt: 'temporary', src: 'hello.png' },
-  { id: 5, alt: 'temporary', src: 'test.svg' },
-  { id: 6, alt: 'temporary', src: 'test1.svg' }
+  { id: 1, alt: 'temporary', src: 'carouselImage.svg' },
+  { id: 2, alt: 'temporary', src: 'carouselImage.svg' },
+  { id: 3, alt: 'temporary', src: 'carouselImg.svg' },
+  { id: 4, alt: 'temporary', src: 'carouselImage.svg' },
+  { id: 5, alt: 'temporary', src: 'carouselImg.svg' },
+  { id: 6, alt: 'temporary', src: 'carouselImage.svg' }
 ];
 
 export const logocarousel: LogoCarouselType[] = [
