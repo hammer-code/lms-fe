8dbb0642dd79d97bf2a53623b904cffa7ec9d77e
Farhan <anak.upload90@gmail.com>
2024-03-13 21:40:11 +0800

    feat: Create my course page

---
+++ src/app/dashboard/mycourse/page.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+import { FC } from 'react';
+
+const MyCourse: FC<NextPage> = () => {
+  return <h1 className="text-2xl font-semibold">My Course Page</h1>;
+};
+export default MyCourse;
