29b3eb44a86e7e13b3c8de0c4bc492df52526eea
Farhan <anak.upload90@gmail.com>
2024-03-13 22:01:19 +0800

    feat: Update MainMenu constant

--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -1,5 +1,11 @@
 import { DashboardMenuType, MainMenuType } from '@/types';
-import { Mail, NotebookPen } from 'lucide-react';
+import {
+  Home,
+  GraduationCap,
+  BookOpen,
+  ClipboardList,
+  ScrollText
+} from 'lucide-react';
 
 export const MainMenu: MainMenuType[] = [
   {
@@ -28,15 +34,36 @@ export const DashboardMenu: DashboardMenuType[] = [
   {
     id: 1,
     title: 'Dashboard',
-    icon: Mail,
+    icon: Home,
     path: '/dashboard',
     isChildren: false
   },
   {
     id: 2,
     title: 'Course',
-    icon: NotebookPen,
+    icon: GraduationCap,
     path: '/dashboard/course',
+    isChildren: false
+  },
+  {
+    id: 3,
+    title: 'My Course',
+    icon: BookOpen,
+    path: '/dashboard/mycourse',
+    isChildren: false
+  },
+  {
+    id: 4,
+    title: 'Assignment',
+    icon: ClipboardList,
+    path: '/dashboard/assignment',
+    isChildren: false
+  },
+  {
+    id: 5,
+    title: 'Certification',
+    icon: ScrollText,
+    path: '/dashboard/certification',
     isChildren: false
   }
 ];
