6539ccdcbafb50fcdad6afc2968f17a368f36a38
mpsalunggg <putrasatria893@gmail.com>
2024-03-14 13:25:42 +0800

    chore: change naming AuthService

--- src/features/Auth/hooks/index.ts
+++ src/features/Auth/hooks/index.ts
@@ -1,13 +1,13 @@
 import { RegisterType } from '@/domains/auth';
 import useToast from '@/hooks/useToast';
-import { AuthService } from '@/services/auth';
+import { RegisterService } from '@/services/auth';
 import { useMutation } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 
 export const useAuthRegister = () => {
   const { showToast } = useToast();
   return useMutation({
-    mutationFn: async (values: RegisterType) => await AuthService(values),
+    mutationFn: async (values: RegisterType) => await RegisterService(values),
     onSuccess: () => {
       showToast({
         title: 'Registration Successfuly',
