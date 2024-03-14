9b30c364fdfecc66b0e794b9f1c2238b6fcfe0a4
zkyzzz <muhadlim17@gmail.com>
2024-03-15 07:03:47 +0800

    feat: Redirect to Sign In page when token expired

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,6 +1,15 @@
 export const setToken = async (token: string) => {
   const date = new Date();
-  date.setTime(date.getTime() + 1 * 24 * 60 * 60 * 1000);
+  date.setTime(date.getTime() + 10 * 1000);
 
   document.cookie = 'token' + '=' + token + '; expires=' + date.toUTCString();
+};
+
+export const getCookie = (name: string) => {
+  const value = '; ' + document.cookie;
+  const parts = value.split('; ' + name + '=');
+
+  if (parts.length == 2) {
+    return parts.pop()?.split(';').shift();
+  }
 };
