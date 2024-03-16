50d98fb654002ff05ccba383921db9c8589faf66
mpsalunggg <putrasatria893@gmail.com>
2024-03-17 06:09:11 +0800

    fix: bug on getCookie

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,15 +1,18 @@
-export const setToken = async (token: string) => {
+export const setToken = (token: string) => {
   const date = new Date();
-  date.setTime(date.getTime() + 10 * 1000);
+  date.setTime(date.getTime() + 12 * 60 * 60 * 1000);
+  // date.setTime(date.getTime() + 5 * 1000);
 
   document.cookie = 'token' + '=' + token + '; expires=' + date.toUTCString();
 };
 
 export const getCookie = (name: string) => {
-  const value = '; ' + document.cookie;
-  const parts = value.split('; ' + name + '=');
-
-  if (parts.length == 2) {
-    return parts.pop()?.split(';').shift();
+  if (typeof document !== 'undefined') {
+    const value = '; ' + document.cookie;
+    const parts = value.split('; ' + name + '=');
+    if (parts.length === 2) {
+      return parts.pop()?.split(';').shift();
+    }
   }
+  return null;
 };
