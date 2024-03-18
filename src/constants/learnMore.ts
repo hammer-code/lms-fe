cd21f0eddff3edde8cba7ba5d1f438155ef84863
zkyzzz <muhadlim17@gmail.com>
2024-03-19 07:51:05 +0800

    chore: Cleanup unused constant and type

--- src/constants/learnMore.ts
+++
@@ -1,7 +0,0 @@
-import { LearnMoreType } from '@/types';
-import { ArrowRightIcon } from '@radix-ui/react-icons';
-
-export const LearnMoreButton: LearnMoreType = {
-  icon: ArrowRightIcon,
-  alt: 'arrow right icon'
-};
