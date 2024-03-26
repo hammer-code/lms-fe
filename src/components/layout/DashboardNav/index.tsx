43f080df384073ebeb6f4f793c0d666a8d60b340
mpsalunggg <putrasatria893@gmail.com>
2024-03-27 06:55:25 +0800

    feat: create dropdown on navbar dashboard

--- src/components/layout/DashboardNav/index.tsx
+++ src/components/layout/DashboardNav/index.tsx
@@ -1,5 +1,12 @@
 import HmcLogo from '@/components/common/HmcLogo';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger
+} from '@/components/ui/popover';
+import { LockIcon, User2 } from 'lucide-react';
+import Link from 'next/link';
 import MobileSide from '../MobileSide';
 import ThemeToggle from '../ThemeToggle';
 
@@ -14,13 +21,28 @@ const DashboardNav = () => {
           <MobileSide />
         </div>
         <div className="flex items-center gap-2">
-          <div className="flex items-center gap-2">
-            <p className="text-sm">Anonymous</p>
-            <Avatar className="w-8 h-8">
-              <AvatarImage src="https://i.pinimg.com/736x/f4/e1/fe/f4e1fe45793230225fc253a1c0ce251f.jpg" />
-              <AvatarFallback>Anonymous</AvatarFallback>
-            </Avatar>
-          </div>
+          <p className="text-sm">Anonymous</p>
+          <Popover>
+            <PopoverTrigger>
+              <Avatar className="w-8 h-8">
+                <AvatarFallback>Anonymous</AvatarFallback>
+                <AvatarImage src="https://i.pinimg.com/736x/f4/e1/fe/f4e1fe45793230225fc253a1c0ce251f.jpg" />
+              </Avatar>
+            </PopoverTrigger>
+            <PopoverContent className="mt-5 space-y-2 p-3">
+              <Link
+                href="/dashboard/profile"
+                className="p-2 flex items-center gap-3 hover:bg-secondary/80 rounded-sm"
+              >
+                <User2 />
+                <p className="text-sm cursor-pointer">Profile</p>
+              </Link>
+              <div className="p-2 flex items-center gap-3 hover:bg-secondary/80 rounded-sm">
+                <LockIcon />
+                <p className="text-sm cursor-pointer">Logout</p>
+              </div>
+            </PopoverContent>
+          </Popover>
           <ThemeToggle />
         </div>
       </nav>
