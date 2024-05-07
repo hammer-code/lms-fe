53f27d7c696940d265ffe67d18035b816b9507b8
adzaky <muhadlim17@gmail.com>
2024-05-07 15:03:53 +0800

    feat(main): adjust testimonial section title responsive

--- src/features/Main/sections/Testimonial/index.tsx
+++ src/features/Main/sections/Testimonial/index.tsx
@@ -8,10 +8,10 @@ const Testimonial: FC = () => {
       className="container min-h-screen flex flex-col gap-8 px-4"
     >
       <div className="space-y-2">
-        <h1 className="text-2xl text-hmc-base text-center md:text-start font-semibold">
+        <h1 className="text-2xl md:text-4xl text-hmc-base text-center md:text-start font-semibold">
           Testimonial
         </h1>
-        <h2 className="text-3xl text-hmc-dark text-center md:text-start">
+        <h2 className="text-3xl md:text-5xl text-hmc-dark text-center md:text-start font-medium">
           Skilline has got more than 100k ratings{' '}
         </h2>
       </div>
