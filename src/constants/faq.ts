ddcde055ccdcd425974ca007c75424095daf4f7a
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-05-12 09:08:41 +0800

    create several questions and answers about HMC Learn for FAQs

--- src/constants/faq.ts
+++ src/constants/faq.ts
@@ -3,17 +3,27 @@ import { FAQType } from '@/types';
 export const FAQ: FAQType[] = [
   {
     id: 'item-1',
-    question: 'Is this a question?',
-    answer: 'Yes, and this is the answer.'
+    question: 'What is HMC Learn?',
+    answer: 'HMC Learn is a learning management system of HammerCode Community that provides several programming courses. With HMC Learn, you are able to access and take the courses that you need in the field of programming.'
   },
   {
     id: 'item-2',
-    question: 'Is this a question?',
-    answer: 'Yes, and this is the answer.'
+    question: 'Who is eligible to enroll in course at HMC Learn?',
+    answer: 'The course is open to individuals within the age range of 18 to 35 years.'
   },
   {
     id: 'item-3',
-    question: 'Is this a question?',
-    answer: 'Yes, and this is the answer.'
-  }
+    question: 'Are there any fees for joining the course that offered by HMC Learn?',
+    answer: 'Yes, there are fees for joining the course that offered by HMC Learn.'
+  },
+  {
+    id: 'item-4',
+    question: 'How to register for joining the course that offered by HMC Learn?',
+    answer: "Click the 'Sign up' button to create your account if you dont have an account yet. Then, Complete your personal details, then click 'Sign Up'." 
+  },
+  {
+    id: 'item-5',
+    question: 'How do I pay for the course?',
+    answer: "There are several payment methods that can be used, GoPay, OVO, Virtual Account, and Bank Transfer."
+  },
 ];
